d367fa9825d9dd06e90d1111b56540de7892e844
kdedeurw <66272745+kdedeurw@users.noreply.github.com>
2020-06-01 20:53:49 +0200

    Update #1
    - updated game loop
    - added Timer class
    - updated Transform class

--- Minigin/Minigin.cpp
+++ Minigin/Minigin.cpp
@@ -1,18 +1,36 @@
+//Base includes
 #include "MiniginPCH.h"
 #include "Minigin.h"
 #include <chrono>
 #include <thread>
-#include "InputManager.h"
+#include "Transform.h"
+#include "GlobalInput.h"
 #include "SceneManager.h"
 #include "Renderer.h"
 #include "ResourceManager.h"
 #include <SDL.h>
-#include "TextObject.h"
 #include "GameObject.h"
 #include "Scene.h"
 
+//Project includes
+#include "Timer.h"
+#include "Components.h"
+#include "GameState.h"
+
 using namespace std;
 using namespace std::chrono;
+
+dae::Minigin::Minigin()
+	: m_GameState{ GameState::GetInstance() }
+	, m_SceneManager{ SceneManager::GetInstance() }
+	, m_Renderer{ Renderer::GetInstance() }
+	, m_ResourceManager{ ResourceManager::GetInstance() }
+	, m_GlobalInput{ GlobalInput::GetInstance() }
+	, m_GlobalMemoryPools{ GlobalMemoryPools::GetInstance() }
+{}
+
+dae::Minigin::~Minigin()
+{}
 
 void dae::Minigin::Initialize()
 {
@@ -34,66 +52,248 @@ void dae::Minigin::Initialize()
 		throw std::runtime_error(std::string("SDL_CreateWindow Error: ") + SDL_GetError());
 	}
 
-	Renderer::GetInstance().Init(m_Window);
+	m_Renderer.Init(m_Window);
 }
 
 /**
  * Code constructing the scene world starts here
  */
-void dae::Minigin::LoadGame() const
+void dae::Minigin::LoadGame()
 {
-	auto& scene = SceneManager::GetInstance().CreateScene("Demo");
+	Scene& scene = m_SceneManager.CreateScene("Demo");
 
-	auto go = std::make_shared<GameObject>();
-	go->SetTexture("background.jpg");
-	scene.Add(go);
+	GameObject* pGo = m_GlobalMemoryPools.CreateGameObject();
+	Texture2DComponent* pTex = m_GlobalMemoryPools.CreateComponent<Texture2DComponent>();
+	pTex->SetTexture("background.jpg");
+	pGo->AddComponent(pTex);
+	pGo->GetTransform().SetPosition(320, 240);
+	scene.AddObject(pGo);
 
-	go = std::make_shared<GameObject>();
-	go->SetTexture("logo.png");
-	go->SetPosition(216, 180);
-	scene.Add(go);
+	pGo = m_GlobalMemoryPools.CreateGameObject();
+	pTex = m_GlobalMemoryPools.CreateComponent<Texture2DComponent>();
+	pGo->AddComponent(pTex);
+	pTex->SetTexture("logo.png");
+	pGo->GetTransform().SetPosition(320, 200);
+	scene.AddObject(pGo);
 
-	auto font = ResourceManager::GetInstance().LoadFont("Lingua.otf", 36);
-	auto to = std::make_shared<TextObject>("Programming 4 Assignment", font);
-	to->SetPosition(80, 20);
-	scene.Add(to);
+	Font* pFont = m_ResourceManager.LoadFont("Lingua.otf", 36);
+	TextObject* pTo = m_GlobalMemoryPools.CreateTextObject("Programming 4 Assignment", pFont);
+	pTo->GetTransform().SetPosition(80, 20);
+	scene.AddObject(pTo);
 }
 
 void dae::Minigin::Cleanup()
 {
-	Renderer::GetInstance().Destroy();
 	SDL_DestroyWindow(m_Window);
 	m_Window = nullptr;
 	SDL_Quit();
 }
 
-void dae::Minigin::Run()
+void dae::Minigin::Start()
 {
 	Initialize();
 
 	// tell the resource manager where he can find the game data
-	ResourceManager::GetInstance().Init("../Data/");
+	m_ResourceManager.Init("../Data/");
 
 	LoadGame();
+	AddFPSScene();
+	GameObject* pParent = ParentChildTest();
+
+	Scene& scene = m_SceneManager.CreateScene("InputTestScene");
+	TextObject* pInputTo = m_GlobalMemoryPools.CreateTextObject("", m_ResourceManager.LoadFont("Lingua.otf", 36));
+	pInputTo->GetTransform().SetPosition(320, 280);
+	pInputTo->SetText("No input");
+	scene.AddObject(pInputTo);
+	TextObject* pTo = m_GlobalMemoryPools.CreateTextObject("", m_ResourceManager.LoadFont("Lingua.otf", 36));
+	pTo->SetText("MouseButtons to change width");
+	pTo->GetTransform().SetPosition(0.f, 350.f);
+	scene.AddObject(pTo);
+	pTo = m_GlobalMemoryPools.CreateTextObject("", m_ResourceManager.LoadFont("Lingua.otf", 36));
+	pTo->SetText("Scrollwheel to change height");
+	pTo->GetTransform().SetPosition(0.f, 400.f);
+	scene.AddObject(pTo);
+	pTo = m_GlobalMemoryPools.CreateTextObject("", m_ResourceManager.LoadFont("Lingua.otf", 36));
+	pTo->SetText("WASD to move");
+	pTo->GetTransform().SetPosition(0.f, 450.f);
+	scene.AddObject(pTo);
+	GameObject* pGo = m_GlobalMemoryPools.CreateGameObject();
+	Texture2DComponent* pTex = m_GlobalMemoryPools.CreateComponent<Texture2DComponent>();
+	pTex->SetTexture("logo.png");
+	pGo->AddComponent(pTex);
+	scene.AddObject(pGo);
+
+	Vector4& dest{ pTex->GetDestRect() };
+	//Vector4& src{ pTex->GetSourceRect() };
+	Vector2& scale{ pGo->GetTransform().GetScale() };
+	float& rot{ pGo->GetTransform().GetRotation() };
 
 	{
-		auto& renderer = Renderer::GetInstance();
-		auto& sceneManager = SceneManager::GetInstance();
-		auto& input = InputManager::GetInstance();
-
-		bool doContinue = true;
-		while (doContinue)
+		float lag{};
+		Timer<high_resolution_clock> timer{}; //start point automatically resets to Time::Now();
+		while (!m_IsQuit)
 		{
+			//forced sleeptime
+			//TODO: fix this goddamn time issue
+			this_thread::sleep_for(16ms);
+			//process all input
+			m_GlobalInput.KeyboardMouseListener.ProcessInput();
+			m_GlobalInput.ControllerListener.ProcessInput();
+			m_IsQuit = m_GlobalInput.KeyboardMouseListener.IsQuit();
+			//force quit
+			m_IsQuit = m_GlobalInput.KeyboardMouseListener.IsPressed(Key::Escape);
+			//get Time::Now()
 			const auto currentTime = high_resolution_clock::now();
+			//set global deltatime
+			m_GameState.DeltaTime = timer.GetElapsedTime<float, milliseconds>();
+
+			InputTest(pInputTo);
+
+			pGo->GetTransform().SetPosition(m_GlobalInput.KeyboardMouseListener.GetMousePos());
+			pGo->GetTransform().SetScale(scale);
+			pGo->GetTransform().SetRotation(rot);
+			Texture2DComponent* pTest = pGo->GetComponent<Texture2DComponent>();
+
+			//DestRect test
+			{
+				if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::A))
+					dest.x -= 1.f;
+				else if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::D))
+					dest.x += 1.f;
+				if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::W))
+					dest.y -= 1.f;
+				else if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::S))
+					dest.y += 1.f;
+
+				if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::ArrowLeft))
+					dest.z -= 1.f;
+				else if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::ArrowRight))
+					dest.z += 1.f;
+				if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::ArrowUp))
+					dest.w += 1.f;
+				else if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::ArrowDown))
+					dest.w -= 1.f;
+			}
+
+			ScaleTest(scale);
+
+			rot += 1.f;
+
+			//pTest->SetSourceRect(src);
+			pTest->SetDestRect(dest);
+
+			pParent->GetTransform().SetPosition(m_GlobalInput.KeyboardMouseListener.GetMousePos() + Vector2{ 100.f, 0.f });
+			pParent->GetTransform().Rotate(1.f);
+			pParent->GetChildren().front()->GetTransform().Rotate(2.f);
+			//pParent->GetChildren().front()->GetChildren().front()->GetTransform().Rotate(2.f);
+
+			lag += m_GameState.DeltaTime;
+			while(lag >= MsPerFrame)
+			{
+				m_GameState.DeltaTime = MsPerFrame / 1000.f;
+				m_SceneManager.Update();
+				lag -= MsPerFrame;
+			}
+			m_Renderer.Render();
+			timer.ResetStartTime();
 			
-			doContinue = input.ProcessInput();
-			sceneManager.Update();
-			renderer.Render();
-			
-			auto sleepTime = duration_cast<duration<float>>(currentTime + milliseconds(MsPerFrame) - high_resolution_clock::now());
+			const auto sleepTime = duration_cast<duration<float>>(currentTime + milliseconds(MsPerFrame) - high_resolution_clock::now());
 			this_thread::sleep_for(sleepTime);
 		}
 	}
 
 	Cleanup();
+}
+
+void dae::Minigin::ForceEnd()
+{
+	m_IsQuit = true;
+}
+
+void dae::Minigin::AddFPSScene() const
+{
+	Scene& scene = m_SceneManager.CreateScene("FPS");
+	GameObject* pGo = m_GlobalMemoryPools.CreateGameObject();
+	FPSComponent* pFPS = m_GlobalMemoryPools.CreateComponent<FPSComponent>();
+	pGo->AddComponent(pFPS);
+	scene.AddObject(pGo);
+}
+
+void dae::Minigin::InputTest(TextObject* pTo)
+{
+	pTo->SetText("No input");
+	if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::W))
+	{
+		pTo->SetText("W");
+		if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::A))
+			pTo->SetText("W + A");
+		else if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::D))
+			pTo->SetText("W + D");
+		pTo->GetTransform().SetPosition(pTo->GetTransform().GetPosition() - Vector3{ 0.f, 1.f });
+	}
+	else if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::S))
+	{
+		pTo->SetText("S");
+		if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::A))
+			pTo->SetText("S + A");
+		else if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::D))
+			pTo->SetText("S + D");
+		pTo->GetTransform().SetPosition(pTo->GetTransform().GetPosition() + Vector3{ 0.f, 1.f });
+	}
+	if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::A))
+	{
+		pTo->SetText("A");
+		if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::W))
+			pTo->SetText("W + A");
+		else if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::S))
+			pTo->SetText("S + A");
+		pTo->GetTransform().SetPosition(pTo->GetTransform().GetPosition() - Vector3{ 1.f, 0.f });
+	}
+	else if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::D))
+	{
+		pTo->SetText("D");
+		if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::W))
+			pTo->SetText("W + D");
+		else if (m_GlobalInput.KeyboardMouseListener.IsPressed(Key::S))
+			pTo->SetText("S + D");
+		pTo->GetTransform().SetPosition(pTo->GetTransform().GetPosition() + Vector3{ 1.f, 0.f });
+	}
+}
+
+void dae::Minigin::ScaleTest(Vector2& scale)
+{
+	if (m_GlobalInput.KeyboardMouseListener.IsScrollWheelMove(ScrollWheel::Up))
+		scale.x += 0.1f;
+	else if (m_GlobalInput.KeyboardMouseListener.IsScrollWheelMove(ScrollWheel::Down))
+		scale.x -= 0.1f;
+
+	if (m_GlobalInput.KeyboardMouseListener.IsMouseButtonPressed(Mouse::LMB))
+		scale.y += 0.1f;
+	else if (m_GlobalInput.KeyboardMouseListener.IsMouseButtonPressed(Mouse::RMB))
+		scale.y -= 0.1f;
+}
+
+GameObject* dae::Minigin::ParentChildTest()
+{
+	Scene& scene = m_SceneManager.CreateScene("ParentChild");
+	GameObject* pParent = m_GlobalMemoryPools.CreateGameObject();
+	Texture2DComponent* pTex = m_GlobalMemoryPools.CreateComponent<Texture2DComponent>();
+	pTex->SetTexture("logo.png");
+	pParent->AddComponent(pTex);
+	pParent->GetTransform().SetPosition(100.f, 100.f);
+	GameObject* pChild = m_GlobalMemoryPools.CreateGameObject();
+	pTex = m_GlobalMemoryPools.CreateComponent<Texture2DComponent>();
+	pTex->SetTexture("logo.png");
+	pChild->AddComponent(pTex);
+	pChild->GetTransform().SetPosition(100.f, 100.f);
+	//GameObject* pPivot = m_GlobalMemoryPools.CreateGameObject();
+	//pPivot->AddChildObject(pChild);
+	//pParent->AddChildObject(pPivot);
+	pParent->AddChildObject(pChild);
+	pParent->GetTransform().SetRotation(90.f);
+	//pParent->GetTransform().SetScale(1.5f, 0.5f);
+	//pChild->GetTransform().SetScale(0.5f, 1.5f);
+	pChild->GetTransform().SetRotation(180.f);
+	scene.AddObject(pParent);
+	return pParent;
 }
--- Minigin/Minigin.h
+++ Minigin/Minigin.h
@@ -1,16 +1,48 @@
 #pragma once
 struct SDL_Window;
+
+class GameState;
+class SceneManager;
+class Renderer;
+class ResourceManager;
+class GlobalInput;
+class GlobalMemoryPools;
+class TextObject;
+struct Vector2;
+class GameObject;
 namespace dae
 {
 	class Minigin
 	{
 	public:
-		void Initialize();
-		void LoadGame() const;
-		void Cleanup();
-		void Run();
+		Minigin();
+		~Minigin();
+
+		void Start();
+		void ForceEnd();
+
 	private:
 		static const int MsPerFrame = 16; //16 for 60 fps, 33 for 30 fps
 		SDL_Window* m_Window{};
+		bool m_IsQuit = false;
+
+		//decided to leave these as singletons
+		GameState& m_GameState; //waste of time and code bc singleton and everything in it is a singleton apart from DeltaTime
+		SceneManager& m_SceneManager;
+		Renderer& m_Renderer;
+		ResourceManager& m_ResourceManager;
+		GlobalInput& m_GlobalInput;
+		GlobalMemoryPools& m_GlobalMemoryPools;
+		//the above wastes extra space bc they're singletons anyway
+
+		//functions
+		void Initialize();
+		void LoadGame();
+		void Cleanup();
+
+		void AddFPSScene() const;
+		void InputTest(TextObject* pTo);
+		void ScaleTest(Vector2& scale);
+		GameObject* ParentChildTest();
 	};
 }
